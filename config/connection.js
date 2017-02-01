ecedd157bcca2a3079b7748cb9173d892f383ee8
Emazing1 <Emazing24@gmail.com>
2017-01-31 23:25:02 -0500

    jaws

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,19 @@
-//connection to database and export to be used for orm
+
 //this was adjusted from the original to include jaws
+
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "4742Cire",
-  database: "burgers_db"
-});
+var connection;
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+    mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '4742Cire',
+    database: 'burgers_db'
+  });
+};
 
-// Export connection for our ORM to use.
+connection.connect();
 module.exports = connection;
